0fd65bf0ea31e7956bbf23fdd845d867f16761e7
FernandoZe <124936632+FernandoZe@users.noreply.github.com>
2023-04-20 14:31:26 -0600

    Add files via upload

---
+++ pruebacatalogo.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Catálogo de productos</title>
+	<style>
+		table {
+			border-collapse: collapse;
+			width: 100%;
+		}
+		td {
+			border: 1px solid #ccc;
+			padding: 10px;
+			text-align: center;
+			vertical-align: middle;
+			width: 20%;
+		}
+		img {
+			max-width: 100%;
+			height: auto;
+		}
+	</style>
+</head>
+<body>
+	<table>
+		<tr>
+			<td>
+				<img src="producto1.jpg" alt="Producto 1">
+				<h3>Producto 1</h3>
+				<p>Precio: $10</p>
+				<button>Comprar</button>
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
